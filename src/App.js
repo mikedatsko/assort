5e26221e3198d9f239ae2180ed877ad0b164faa1
Mykhailo Datsko <mike@datsko.it>
2017-09-18 14:02:46 +0300

    initial setup, added react, added express

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './assets/images/logo-long.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="App-slogan">Web Development Services</div>
+          <div className="App-contacts">
+            <span className="icon-mail"></span> <a href="mailto:info@assort.top">info@assort.top</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
